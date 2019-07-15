20c417e8c11fee74945ccc90416feb9bc4b2c882
legiorex <batyushev@gmail.com>
2019-07-15 23:19:07 +1000

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+// module.exports = (config) => require('react-app-rewire-postcss')(config, {});
+module.exports = (config) => require('react-app-rewire-postcss')(config, {
+  plugins () {
+    return [
+      require('postcss-preset-env')({
+        stage: 0,
+      }),
+      require('autoprefixer')
+    ];
+  },
+});
---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+
+
+// module.exports = {
+//   plugins: {
+//     'postcss-preset-env': {
+//       stage: 0,
+//     },
+//     'autoprefixer': true,
+
+//   },
+// };
+// module.exports = {
+//   plugins: [
+//     require('autoprefixer')
+//   ],
+// };
